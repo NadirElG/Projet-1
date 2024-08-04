c0134dbe58f0ebf67f9e725b59c8062957e6e606
NadirElG <nadirelghammaz@gmail.com>
2024-08-04 22:17:46 +0200

    SloTeam[MultiAuth] : seeders(users)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('image')->nullable();
+            $table->string('username')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
+            $table->enum('role',['admin','coach','user'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin user',
+                'username' => 'admin-1',
+                'email' => 'admin@sloteam.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin'
+            ],
+            [
+                'name' => 'Coach user',
+                'username' => 'Coach Nadir',
+                'email' => 'coach@sloteam.com',
+                'password' => bcrypt('password'),
+                'role' => 'coach'
+            ],
+            [
+                'name' => 'User user',
+                'username' => 'User Alpha',
+                'email' => 'user@sloteam.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin'
+            ]
+            ]);
+    }
+}
