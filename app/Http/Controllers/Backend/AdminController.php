59efd9d1585d187ca9e0537e80a855d8728deca9
NadirElG <nadirelghammaz@gmail.com>
2024-08-06 16:38:05 +0200

    SloTeam[MultiAuth] : Init-Admin LoginPage

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -11,5 +11,10 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
+
+    public function login()
+    {
+        return view('admin.auth.login');
+    }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+Route::get('admin/login',[AdminController::class, 'login'])->name('admin.login');
+
