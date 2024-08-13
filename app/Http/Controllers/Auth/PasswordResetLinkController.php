d364e5485f697ed177fd766fb3efea5c74492b7e
NadirElG <nadirelghammaz@gmail.com>
2024-08-13 19:05:50 +0200

    SloTeam[FrontendSetup] : Reset-Password(Mastering+MAILTRAPSetup)

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,6 +36,8 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        toastr('Reset link has been send');
+
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
