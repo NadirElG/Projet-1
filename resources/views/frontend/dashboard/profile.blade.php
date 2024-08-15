7f0f122512e0313ef48ef9d53b09d98a7298703c
NadirElG <nadirelghammaz@gmail.com>
2024-08-16 00:06:04 +0200

    SloTeam[UserProfile] : frontend update

--- resources/views/frontend/dashboard/profile.blade.php
+++ resources/views/frontend/dashboard/profile.blade.php
@@ -13,69 +13,31 @@
             <div class="wsus__dashboard_profile">
               <div class="wsus__dash_pro_area">
                 <h4>basic information</h4>
-                <form>
-                  <div class="row">
-                    <div class="col-xl-9">
-                      <div class="row">
-                        <div class="col-xl-6 col-md-6">
+
+                    <div class="col-md-12">
+                        <div class="col-md-2">
+                            <div class="wsus__dash_pro_img">
+                              <img src="{{ asset('frontend/images/ts-2.jpg') }}" alt="img" class="img-fluid w-100">
+                              <input type="file">
+                            </div>
+                          </div>
+
+                        <div class="col-md-12 mt-5">
                           <div class="wsus__dash_pro_single">
                             <i class="fas fa-user-tie"></i>
-                            <input type="text" placeholder="First Name">
+                            <input type="text" placeholder="Name">
                           </div>
                         </div>
-                        <div class="col-xl-6 col-md-6">
-                          <div class="wsus__dash_pro_single">
-                            <i class="fas fa-user-tie"></i>
-                            <input type="text" placeholder="Last Name">
-                          </div>
-                        </div>
-                        <div class="col-xl-6 col-md-6">
-                          <div class="wsus__dash_pro_single">
-                            <i class="far fa-phone-alt"></i>
-                            <input type="text" placeholder="Phone">
-                          </div>
-                        </div>
-                        <div class="col-xl-6 col-md-6">
+
+                        <div class="col-md-12">
                           <div class="wsus__dash_pro_single">
                             <i class="fal fa-envelope-open"></i>
                             <input type="email" placeholder="Email">
                           </div>
                         </div>
-                        <div class="col-xl-12">
-                          <div class="wsus__dash_pro_single">
-                            <textarea cols="3" rows="5" placeholder="About You"></textarea>
-                          </div>
-                        </div>
-                        <div class="col-xl-12">
-                          <div id="medicine_row3">
-                            <div class="row">
-                              <div class="col-xl-5 col-md-5">
-                                <div class="medicine_row_input">
-                                  <input type="text" placeholder="www.facebook.com" name="name[]">
-                                </div>
-                              </div>
-                              <div class="col-xl-5 col-md-5">
-                                <div class="medicine_row_input">
-                                  <input type="text" placeholder="www.youtube.com" name="name[]">
-                                </div>
-                              </div>
-                              <div class="col-xl-2 col-md-2">
-                                <div class="medicine_row_input">
-                                  <button type="button" id="add_row3"><i class="fas fa-plus"
-                                      aria-hidden="true"></i></button>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+
                     </div>
-                    <div class="col-xl-3 col-sm-6 col-md-6">
-                      <div class="wsus__dash_pro_img">
-                        <img src="images/ts-2.jpg" alt="img" class="img-fluid w-100">
-                        <input type="file">
-                      </div>
-                    </div>
+
                     <div class="col-xl-12">
                       <button class="common_btn mb-4 mt-2" type="submit">upload</button>
                     </div>
@@ -104,8 +66,8 @@
                         </div>
                       </div>
                     </div>
-                  </div>
-                </form>
+
+
               </div>
             </div>
           </div>
